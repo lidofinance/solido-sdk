06c6253dc6126fe165406610670afdad0f88a1d5
j-tulforov <jahongir.tulforov@p2p.org>
2022-06-24 14:22:06 +0300

    feat: aliases for better imports

--- src/banner/LidoStakeBanner.tsx
+++ src/banner/LidoStakeBanner.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Box, Text, Link, useBreakpoint } from '@lidofinance/lido-ui';
 
-import LidoLogo from '../assets/lido-logo.inline.svg';
-import Background from '../assets/background.svg';
-import BackgroundVertical from '../assets/background_vertical.svg';
+import LidoLogo from '@/assets/lido-logo.inline.svg';
+import Background from '@/assets/background.svg';
+import BackgroundVertical from '@/assets/background_vertical.svg';
 
-import { getStakeApy, STATIC_DEFAULT_APY } from '../api/stakeApy';
-import { getDefiApy } from '../api/defiApy';
-import { getStakeLink, getDefiLink } from '../utils/getLinkWIthReferrer';
+import { getStakeApy, STATIC_DEFAULT_APY } from '@/api/stakeApy';
+import { getDefiApy } from '@/api/defiApy';
+import { getStakeLink, getDefiLink } from '@/utils/getLinkWIthReferrer';
 import { BannerWrapper } from './styles';
 
 export type Props = {
