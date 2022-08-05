78b3952d055e95b64c2d5e3e62404db895e105bd
edward grolsh <drujininii@yandex.ru>
2022-08-05 09:56:30 +0300

    Revert "fix: delete gatsby file"
    This reverts commit b5a019ccad5de0b279c13b0e09540cc03bba6b2d.

---
+++ src/gatsby-theme-docz/components/Logo/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ReactComponent as StsolLogo } from '@/assets/logo.inline.svg';
+import { Box } from '@lidofinance/lido-ui';
+
+export const Logo = () => (
+  <Box display="flex" alignItems="center">
+    <StsolLogo />
+    <Box ml={10} />
+    Lido on Solana - Frontend SDK
+  </Box>
+);
