59f990a42b13dd9ec8b0d6398f48b1ebb8d2f99b
j-tulforov <jahongir.tulforov@p2p.org>
2022-11-03 20:51:42 +0300

    feat: unit test for exchange rate

---
+++ tests/statistics/getExchangeRate.test.ts
@@ -0,0 +1,26 @@
+import { Connection } from '@solana/web3.js';
+import { SolidoSDK } from '@/index';
+import { getExchangeRate } from '@/statistics/getExchangeRate';
+import { mockValidatorList } from '../helpers/validators';
+
+describe('getExchangeRate', () => {
+  const cluster = 'testnet';
+  let sdk, connection;
+
+  beforeAll(async () => {
+    connection = new Connection('https://api.testnet.solana.com/');
+    sdk = new SolidoSDK(cluster, connection);
+  });
+
+  test('exchange rate returned properties', async () => {
+    mockValidatorList(connection); // making test faster, validator list is heavy
+
+    const { SOLToStSOL, stSOLToSOL } = await sdk.getExchangeRate();
+
+    expect(typeof stSOLToSOL).toBe('number');
+    expect(typeof SOLToStSOL).toBe('number');
+
+    expect(SOLToStSOL).toBeLessThanOrEqual(1);
+    expect(stSOLToSOL).toBeGreaterThanOrEqual(1);
+  });
+});
