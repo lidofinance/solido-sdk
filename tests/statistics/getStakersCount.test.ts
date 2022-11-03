e71df981dd823b83570e7acdee5e760c6da21fcb
j-tulforov <jahongir.tulforov@p2p.org>
2022-11-03 21:32:38 +0300

    feat: unit test for getStakersCount

---
+++ tests/statistics/getStakersCount.test.ts
@@ -0,0 +1,26 @@
+import { Connection } from '@solana/web3.js';
+
+import { SolidoSDK } from '@/index';
+
+describe('getStakersCount', () => {
+  const cluster = 'testnet';
+  let sdk, connection;
+
+  beforeAll(async () => {
+    connection = new Connection('https://api.testnet.solana.com/');
+    sdk = new SolidoSDK(cluster, connection);
+  });
+
+  test('stakersCount accounts quantity', async () => {
+    jest
+      .spyOn(connection, 'getProgramAccounts')
+      .mockReturnValueOnce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) // accountsTotal
+      .mockReturnValueOnce([1, 2, 3, 4, 5]); // accountsEmpty
+
+    const stakersCount = await sdk.getStakersCount();
+
+    expect(stakersCount.accountsTotal).toEqual(10);
+    expect(stakersCount.accountsEmpty).toEqual(5);
+    expect(stakersCount.value).toEqual(5); // accountsTotal - accountsEmpty
+  });
+});
