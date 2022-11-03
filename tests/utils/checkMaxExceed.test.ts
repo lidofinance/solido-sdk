120c0062d2af3ed5f57b85b49a145cfc8f6a8d62
j-tulforov <jahongir.tulforov@p2p.org>
2022-11-03 14:29:38 +0300

    feat: unit test for checkMaxExceed

---
+++ tests/utils/checkMaxExceed.test.ts
@@ -0,0 +1,20 @@
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+
+import { checkMaxExceed } from '@/utils/checkMaxExceed';
+import { ERROR_CODE } from '@/constants';
+
+describe('checkMaxExceed', () => {
+  it('should throw Error if amount is bigger than maxInLamports', () => {
+    try {
+      checkMaxExceed(10, 9.9 * LAMPORTS_PER_SOL);
+    } catch (error) {
+      expect(error.message).toContain('Amount must not exceed MAX');
+      expect(error.code).toEqual(ERROR_CODE.EXCEED_MAX);
+    }
+  });
+
+  it('should not throw Error if amount is less than maxInLamports', () => {
+    const fn = () => checkMaxExceed(9.8, 9.9 * LAMPORTS_PER_SOL);
+    expect(fn).not.toThrowError();
+  });
+});
