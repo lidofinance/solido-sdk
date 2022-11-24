dc0958abf7ed3a31330cfc2df97bd8dd22a62064
j-tulforov <jahongir.tulforov@p2p.org>
2022-11-24 13:21:16 +0300

    feat: update lidoStatistics apy name

--- packages/core/src/statistics/lidoStatistics.ts
+++ packages/core/src/statistics/lidoStatistics.ts
@@ -12,7 +12,7 @@ export async function getLidoStatistics(this: SolidoSDK) {
   ]);
 
   return {
-    apy: stakeApy.max,
+    stakeApy: stakeApy.max,
     totalStaked: {
       value: totalStaked,
       formatted: formatWithCommas(totalStaked),
