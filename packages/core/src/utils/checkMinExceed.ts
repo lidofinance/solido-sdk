00068c738d00beaba1c39d5f4fbfafee947af7e5
exromany <exromany@gmail.com>
2023-05-25 10:42:27 +0300

    feat: lamportsToSolUp

--- packages/core/src/utils/checkMinExceed.ts
+++ packages/core/src/utils/checkMinExceed.ts
@@ -1,12 +1,12 @@
-import { lamportsToSol, solToLamports } from '@/utils/formatters';
-import { ErrorWrapper } from '@common/errorWrapper';
+import { lamportsToSolUp, solToLamports } from '@/utils/formatters';
 import { ERROR_CODE } from '@common/constants';
+import { ErrorWrapper } from '@common/errorWrapper';
 
 export const checkMinExceed = (amount: number, minInLamports: number) => {
   if (solToLamports(amount) <= minInLamports) {
     throw new ErrorWrapper({
       code: ERROR_CODE.EXCEED_MIN,
-      message: `Amount must be greater than rent-exempt fee(${lamportsToSol(minInLamports)})`,
+      message: `Amount must be greater than rent-exempt fee(${lamportsToSolUp(minInLamports)})`,
     });
   }
 };
