fbe9bc83fab01b627c90448f8c81903a316cffab
exromany <exromany@gmail.com>
2024-01-31 15:06:29 +0300

    fix: type stakeApy

--- packages/common/stakeApy.ts
+++ packages/common/stakeApy.ts
@@ -3,7 +3,7 @@ import { ERROR_CODE } from './constants';
 
 export const STATIC_DEFAULT_APY = 0;
 
-// eslint-disable-next-line @typescript-eslint/require-await
-export const getStakeApy = async (): Promise<never> => {
+// eslint-disable-next-line @typescript-eslint/require-await, @typescript-eslint/no-explicit-any
+export const getStakeApy = async (): Promise<any> => {
   throw new ErrorWrapper({ error: new Error('Method deprecated'), code: ERROR_CODE.NO_APY_DATA });
 };
